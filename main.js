d3d9cbd223e6597e6228e13c533f8d6248660368
Jenaya F <145370280+Shava-mp4@users.noreply.github.com>
2024-10-25 09:35:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+// ARRAY VISUALIZER
+
+// Set up Canvas and Graphics Context
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables
+// let myArray = [
+//   200, 500, 350, 400, 580, 170, 225, 325, 100, 550, 250, 300, 600, 50, 275,
+// ];
+// let myArray = [400, 200, 200, 400, 200, 200, 200, 400, 200, 200, 200, 200, 400];
+let myArray = [];
+
+// Main Program Loop
+requestAnimationFrame(draw);
+
+function draw() {
+  // Logic
+  let barWidth = cnv.width / myArray.length;
+
+  // Drawing
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+  // Draw Bar Graph
+  ctx.fillStyle = "orange";
+  ctx.strokeStyle = "grey";
+  for (let i = 0; i < myArray.length; i++) {
+    ctx.fillRect(i * barWidth, cnv.height - myArray[i], barWidth, myArray[i]);
+    ctx.strokeRect(i * barWidth, cnv.height - myArray[i], barWidth, myArray[i]);
+  }
+
+  // Request another Animation Frame
+  requestAnimationFrame(draw);
+}
+
+// --------Random Dance------------
+
+for (let i = 0; i < 100; i++) {
+  myArray.push(300);
+}
+
+// // Key events
+document.addEventListener("keydown", keydownHandler);
+
+// --------Random Dance------------
+function keydownHandler(event) {
+  console.log(event.code);
+
+  if (event.code == "Space") {
+    for (let i = 0; i < myArray.length; i++) {
+      myArray[i] += Math.round(Math.random() * 10 - 5);
+    }
+  } else if (event.code == "KeyR") {
+    for (let i = 0; i < myArray.length; i++) {
+      myArray[i] = 300;
+    }
+  }
+}
+
+// --------Remove Challenge------------
+// function keydownHandler(event) {
+//   console.log(event.code);
+
+//   if (event.code == "Digit1") {
+//     for (let i = 0; i < myArray.length; i++) {
+//       if (myArray[i] == 400) {
+//         myArray.splice(i, 1);
+//         i--;
+//       }
+//     }
+//   } else if (event.code == "Digit2") {
+//     for (let i = 0; i < myArray.length; i++) {
+//       if (myArray[i] == 200) {
+//         myArray.splice(i, 1);
+//         i--;
+//       }
+//     }
+//   }
+// }
